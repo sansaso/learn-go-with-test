d5bba409c530f74bead1acee355d0631ed2324bf
sandross <sandro.salesdesouza@gmail.com>
2022-05-23 11:34:46 -0300

    error handling

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -1,5 +1,10 @@
 package exchange
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Bitcoin int
 
 type Stringer interface {
@@ -10,10 +15,24 @@ type Carteira struct {
 	saldo Bitcoin
 }
 
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+var ErroSaldoInsuficiente = errors.New("não é possível retirar: saldo insuficiente")
+
 func (c *Carteira) Depositar(quantidade Bitcoin) {
 	c.saldo += quantidade
 }
 
 func (c *Carteira) Saldo() Bitcoin {
 	return c.saldo
+}
+
+func (c *Carteira) Retirar(quantidade Bitcoin) error {
+	if quantidade > c.saldo {
+		return (ErroSaldoInsuficiente)
+	}
+	c.saldo -= quantidade
+	return nil
 }
--- exchange/exchange_test.go
+++ exchange/exchange_test.go
@@ -5,15 +5,53 @@ import (
 )
 
 func TestCarteira(t *testing.T) {
-	carteira := Carteira{}
 
-	carteira.Depositar(Bitcoin(10))
+	t.Run("Depositar", func(t *testing.T) {
+		carteira := Carteira{}
+		carteira.Depositar(Bitcoin(10))
+		confirmaSaldo(t, carteira, Bitcoin(10))
+	})
 
+	t.Run("retirar", func(t *testing.T) {
+		carteira := Carteira{saldo: Bitcoin(20)}
+		erro := carteira.Retirar(Bitcoin(10))
+		confirmaSaldo(t, carteira, Bitcoin(10))
+		confirmaErroInexistente(t, erro)
+	})
+
+	t.Run("retirar com saldo insuficiente", func(t *testing.T) {
+		saldoInicial := Bitcoin(20)
+		carteira := Carteira{saldoInicial}
+		erro := carteira.Retirar(Bitcoin(100))
+
+		confirmaSaldo(t, carteira, saldoInicial)
+		confirmaErro(t, erro, ErroSaldoInsuficiente)
+	})
+}
+
+func confirmaSaldo(t *testing.T, carteira Carteira, esperado Bitcoin) {
+	t.Helper()
 	resultado := carteira.Saldo()
 
-	esperado := Bitcoin(10)
+	if resultado != esperado {
+		t.Errorf("resultado %s, esperado %s", resultado, esperado)
+	}
+}
+
+func confirmaErro(t *testing.T, resultado error, esperado error) {
+	t.Helper()
+	if resultado == nil {
+		t.Fatal("Esperava um erro mas nenhum ocorreu")
+	}
 
 	if resultado != esperado {
-		t.Errorf("resultado %d, esperado %d", resultado, esperado)
+		t.Errorf("resultado %s, esperado %s", resultado, esperado)
+	}
+}
+
+func confirmaErroInexistente(t *testing.T, resultado error) {
+	t.Helper()
+	if resultado != nil {
+		t.Fatal("erro inesperado recebido")
 	}
 }
